0ad77e59d8872336231f79d9b3b30c4bd977f7ab
ShinoSpace <22547794+ShinoSpace@users.noreply.github.com>
2020-11-09 01:44:26 +0800

    Update ArcMarginProduct.py

--- margin/ArcMarginProduct.py
+++ margin/ArcMarginProduct.py
@@ -48,7 +48,7 @@ class ArcMarginProduct(nn.Module):
         #one_hot = torch.zeros(cosine.size(), device='cuda' if torch.cuda.is_available() else 'cpu')
         one_hot = torch.zeros_like(cosine)
         one_hot.scatter_(1, label.view(-1, 1), 1)
-        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)   # [[cos(θ_1), cos(θ_2), ..., cos(θ_i + m), ...](1st sample, assume i-th class is the real class), ...]
         output = output * self.s
 
         return output
